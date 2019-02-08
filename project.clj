dab8fb9bbf2786d263a3cb1d63b3501081b2c438
Lucas Viola <lucasmatzenbacher@gmail.com>
2019-02-08 20:57:36 -0200

    Adds ultra and creates test-refresh task for improving dev experience

--- project.clj
+++ project.clj
@@ -5,9 +5,19 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.2"]
                  [ring/ring-defaults "0.2.3"]
-                 [expectations "2.1.1"]]
+                 [expectations "2.2.0-beta1"]]
   :plugins [[lein-ring "0.11.0"]]
   :ring {:handler rest.api.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+                        [ring/ring-mock "0.3.0"]]
+         :plugins [[com.jakemccrary/lein-test-refresh "0.19.0"]]
+                  venantius/ultra "0.5.1"}}
+  :test-refresh {:growl false
+                 :notify-on-success false
+                 :quiet true
+                 :changes-only true
+                 :stack-trace-depth nil
+                 :run-once false
+                 :watch-dirs ["src" "test"]
+                 :refresh-dirs ["src" "test"]})
